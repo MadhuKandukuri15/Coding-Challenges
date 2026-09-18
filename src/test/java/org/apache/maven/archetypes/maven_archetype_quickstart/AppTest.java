6f8a0d80f287c1b382c8d42f55f0c7a188fff17f
MadhuKandukuri15 <kmadhusudhan.15@gmail.com>
2026-09-19 01:17:05 +0530

    first challenge completed

--- src/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/AppTest.java
+++ src/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/AppTest.java
@@ -2,6 +2,8 @@ package org.apache.maven.archetypes.maven_archetype_quickstart;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -9,11 +11,11 @@ import org.junit.jupiter.api.Test;
  */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
+   public static void main(String...arg) {
+	   int arr[] = new int[] {14,54,4,8,784,245};
+	   Arrays.sort(arr);
+	   System.out.println(arr[0]);
+	   System.out.println(arr[arr.length-1]);
+   }
+	
 }
