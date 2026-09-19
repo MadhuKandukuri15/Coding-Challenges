0da3a679f2f6fc5df9ebf9a1700ff81b825c4b78
MadhuKandukuri15 <kmadhusudhan.15@gmail.com>
2026-09-19 22:50:09 +0530

    Reverse number with inbuilt methods and without inbuilt methods

--- src/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/ReverseNumber.java
+++ src/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/ReverseNumber.java
@@ -4,13 +4,34 @@ import java.util.Scanner;
 
 public class ReverseNumber {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NumberFormatException {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter number : ");
-		int num = sc.nextInt();
+		// TODO Auto-generated method stub
+		try {
 
+			int num = sc.nextInt();
+			num = Math.abs(num);
+			// convert number to string array
+			String str = Integer.toString(num);
+			StringBuffer sb = new StringBuffer(str);
+			System.out.println(sb.reverse());
+
+			// without converting in to string
+			int reversed = 0;
+			while (num != 0) {
+				int digit = num % 10; // first ikkada digit teesukuntunna endukante reverse variable ki last lo didit
+										// add cheyadaniki
+				reversed = reversed * 10 + digit;
+				num = num / 10;
+
+			}
+			System.out.println(reversed);
+
+		} finally {
+			System.out.println("finally block executed");
+			sc.close();
+		}
 	}
 
 }
