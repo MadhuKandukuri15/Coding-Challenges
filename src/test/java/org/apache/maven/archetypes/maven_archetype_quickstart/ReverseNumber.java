d183d6da8fbf2f4e4c080ab0464d92e687c8de34
MadhuKandukuri15 <kmadhusudhan.15@gmail.com>
2026-09-19 15:57:21 +0530

    Reverse Number Program

---
+++ src/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/ReverseNumber.java
@@ -0,0 +1,10 @@
+package org.apache.maven.archetypes.maven_archetype_quickstart;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
