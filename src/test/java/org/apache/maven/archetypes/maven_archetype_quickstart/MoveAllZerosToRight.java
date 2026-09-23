115685a624dc2cbae0a7ebe684129f640acc451a
MadhuKandukuri15 <kmadhusudhan.15@gmail.com>
2026-09-23 08:13:25 +0530

    Day 5 Move all zeros to right

--- src/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/MoveAllZerosToRight.java
+++ src/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/MoveAllZerosToRight.java
@@ -7,18 +7,18 @@ public class MoveAllZerosToRight {
 
 		int a[] = new int[] { 1, 0, 4, 0, 5, 0 };
 		int result[] = new int[a.length];
-		int c = 0;
+		int count = 0;
 		for (int i = 0; i < a.length; i++) {
 
 			if (a[i] != 0) {
 
-				result[c] = a[i];
-				++c;
+				result[count] = a[i];
+				++count;
 			}
 
 		}
-		for (int j = c; j < a.length; j++) {
-			result[c] = 0;
+		for (int j = count; j < a.length; j++) {
+			result[count] = 0;
 		}
 		for (int k = 0; k < result.length; k++) {
 			System.out.println(result[k]);
